c4108beb236b88ede7cb8b41d2a8fc3d66316c06
Randy Miller <Randy Miller@DESKTOP-7SQA22L>
2023-03-20 22:29:00 -0400

    Add project files.

---
+++ Working with LINQ Query Syntax/Program.cs
@@ -0,0 +1,34 @@
+﻿class famousPeople
+{
+    public string Name { get; set; }
+    public int? BirthYear { get; set; } = null;
+    public int? DeathYear { get; set; } = null;
+
+
+    IList<famousPeople> stemPeople = new List<famousPeople>() {
+                new famousPeople() { Name= "Michael Faraday", BirthYear=1791,DeathYear=1867 },
+                new famousPeople() { Name= "James Clerk Maxwell", BirthYear=1831,DeathYear=1879 },
+                new famousPeople() { Name= "Marie Skłodowska Curie", BirthYear=1867,DeathYear=1934 },
+                new famousPeople() { Name= "Katherine Johnson", BirthYear=1918,DeathYear=2020 },
+                new famousPeople() { Name= "Jane C. Wright", BirthYear=1919,DeathYear=2013 },
+                new famousPeople() { Name = "Tu YouYou", BirthYear= 1930 },
+                new famousPeople() { Name = "Françoise Barré-Sinoussi", BirthYear=1947 },
+                new famousPeople() {Name = "Lydia Villa-Komaroff", BirthYear=1947},
+                new famousPeople() {Name = "Mae C. Jemison", BirthYear=1956},
+                new famousPeople() {Name = "Stephen Hawking", BirthYear=1942,DeathYear=2018 },
+                new famousPeople() {Name = "Tim Berners-Lee", BirthYear=1955 },
+                new famousPeople() {Name = "Terence Tao", BirthYear=1975 },
+                new famousPeople() {Name = "Florence Nightingale", BirthYear=1820,DeathYear=1910 },
+                new famousPeople() {Name = "George Washington Carver", DeathYear=1943 },
+                new famousPeople() {Name = "Frances Allen", BirthYear=1932,DeathYear=2020 },
+                new famousPeople() {Name = "Bill Gates", BirthYear=1955 }
+     };
+
+    //var countAge = famousPeople.Count(s => s.BirthYear > 1900);
+    var countLetter = from s in famousPeople
+                      where s.Major.Contains("l", "l")
+                      orderby s.Major
+                      select s;
+    var countAge = famousPeople.Count(s => s.BirthYear > 1950);
+
+}
